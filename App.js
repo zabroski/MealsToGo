f278cbcb8f4be1d4b8e716c5d3d4a1bcd1b768f8
zabroski <35234595+zabroski@users.noreply.github.com>
2021-04-10 12:59:42 -0400

    added card looking nice

--- App.js
+++ App.js
@@ -1,24 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, SafeAreaView, Platform, Text, View } from 'react-native';
+import React from "react";
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+
+import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
 
 export default function App() {
   return (
     <>
-      <SafeAreaView style={{flex: 1}}>
-        <View style={{ padding: 16, backgroundColor: "green"}}>
-          <Text>search</Text>
-        </View>
-        <View style={{ flex: 1, padding: 16, backgroundColor: "blue"}}>
-          <Text>list</Text>
-        </View>
-
-      </SafeAreaView>
+      <RestaurantsScreen />
+      <ExpoStatusBar style="auto" />
     </>
-   
   );
 }
-
-const styles = StyleSheet.create({
-
-});
---
+++ src/features/restaurants/components/restaurant-info.component.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Text, StyleSheet } from "react-native";
+import { Card } from "react-native-paper";
+
+export const RestaurantInfo = ({ restaurant = {} }) => {
+  const {
+    name = "some Restaurant",
+    icon,
+    photos = [
+      "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
+    ],
+    address = "235 west 116th strret",
+    isOpenNow = true,
+    rating = 4,
+    isClosedTemporarity,
+  } = restaurant;
+
+  return (
+    <Card elevation={5} style={styles.card}>
+      <Card.Cover key={name} style={styles.cover} source={{ uri: photos[0] }} />
+      <Text>{name}</Text>
+    </Card>
+  );
+};
+
+const styles = StyleSheet.create({
+  card: { backgroundColor: "white" },
+  cover: { padding: 20, backgroundColor: "white" },
+});

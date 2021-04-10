fab07fdc042d5f583342dcc2696cade5d8f12cc9
zabroski <35234595+zabroski@users.noreply.github.com>
2021-04-10 18:57:53 -0400

    style components`

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -3,7 +3,16 @@ import styled from "styled-components/native";
 import { Text, StyleSheet } from "react-native";
 import { Card } from "react-native-paper";
 
-const Title = styled.Text`
+const RestaurantCard = styled(Card)`
+  background-color: white;
+`;
+
+const RestaurantCardCover = styled(Card.Cover)`
+  padding: 20px;
+  background-color: white;
+`;
+
+const Title = styled(Text)`
   padding: 16px;
   color: red;
 `;
@@ -22,10 +31,14 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
   } = restaurant;
 
   return (
-    <Card elevation={5} style={styles.card}>
-      <Card.Cover key={name} style={styles.cover} source={{ uri: photos[0] }} />
+    <RestaurantCard elevation={5} style={styles.card}>
+      <RestaurantCardCover
+        key={name}
+        style={styles.cover}
+        source={{ uri: photos[0] }}
+      />
       <Title>{name}</Title>
-    </Card>
+    </RestaurantCard>
   );
 };
 

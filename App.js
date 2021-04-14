b7e9efdc5e8f209b4dab81b96c06b1507d232cc3
zabroski <35234595+zabroski@users.noreply.github.com>
2021-04-14 18:30:49 -0400

    added Authenticated

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { ThemeProvider } from "styled-components/native";
+import * as firebase from "firebase";
 
 import {
   useFonts as useOswald,
@@ -15,7 +16,36 @@ import { RestaurantsContextProvider } from "./src/services/restaurants/restauran
 import { LocationContextProvider } from "./src/services/location/location.context";
 import { FavouritesContextProvider } from "./src/services/favourites/favourites.context";
 
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyA6yG44tvP8rg50fIvAEBDQe4sML8dOyIg",
+  authDomain: "mealstogo-638a6.firebaseapp.com",
+  projectId: "mealstogo-638a6",
+  storageBucket: "mealstogo-638a6.appspot.com",
+  messagingSenderId: "736424740497",
+  appId: "1:736424740497:web:5919d48b6d300b7153e130",
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
 export default function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  useEffect(() => {
+    setTimeout(() => {
+      firebase
+        .auth()
+        .signInWithEmailAndPassword("raye@gmail.com", "test123")
+        .then((user) => {
+          setIsAuthenticated(true);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    }, 2000);
+  }, []);
+
   const [oswaldLoaded] = useOswald({
     Oswald_400Regular,
   });
@@ -25,6 +55,10 @@ export default function App() {
   });
 
   if (!oswaldLoaded || !latoLoaded) {
+    return null;
+  }
+
+  if (!isAuthenticated) {
     return null;
   }
 

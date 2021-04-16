7995e912f2d39790e005f41716b1fa3cdab7969b
zabroski <35234595+zabroski@users.noreply.github.com>
2021-04-16 16:41:36 -0400

    added backgroond animation and fix setting css

--- src/features/account/screens/account.screen.js
+++ src/features/account/screens/account.screen.js
@@ -1,19 +1,29 @@
 import React from "react";
+import LottieView from "lottie-react-native";
 
 import { Spacer } from "../../../components/spacer/spacer.component";
-
 import {
   AccountBackground,
   AccountContainer,
   AccountCover,
   AuthButton,
   Title,
+  AnimationWrapper,
 } from "../components/account.styles";
 
 export const AccountScreen = ({ navigation }) => {
   return (
     <AccountBackground>
       <AccountCover />
+      <AnimationWrapper>
+        <LottieView
+          key="animation"
+          autoPlay
+          loop
+          resizeMode="cover"
+          source={require("../../../../assets/watermelon.json")}
+        />
+      </AnimationWrapper>
       <Title>Meals To Go</Title>
       <AccountContainer>
         <AuthButton
